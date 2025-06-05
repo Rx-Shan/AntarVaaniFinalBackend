79f3a94df1edb7aac2515f117b8b522e4fe8767e
RxShan <rxpaul2002work@gmail.com>
2025-06-05 16:59:48 +0530

    Port to 10000

--- app/main.py
+++ app/main.py
@@ -86,4 +86,4 @@ def chat_with_bot(data: ChatRequest):
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 8000))  # use env PORT or default 8000
-    uvicorn.run("app.main:app", host="0.0.0.0", port=port)  # <-- use the variable here!
+    uvicorn.run("app.main:app", host="0.0.0.0", port=10000)  # <-- use the variable here!
